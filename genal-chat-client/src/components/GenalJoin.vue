59c168be4c00df23138405b917474b6176d4f5af
黄汉增 <huanghanzeng@dm-ai.cn>
2020-07-19 19:07:10 +0800

    #8_用户首次登陆无法改名问题

--- genal-chat-client/src/components/GenalJoin.vue
+++ genal-chat-client/src/components/GenalJoin.vue
@@ -87,6 +87,8 @@ export default class GenalJoin extends Vue {
         if(this.type === 'regist') {
           values.createTime = new Date().valueOf()
         }
+        // @ts-ignore
+        delete values.remember
         this.$emit(this.type,values)
       }
     });
