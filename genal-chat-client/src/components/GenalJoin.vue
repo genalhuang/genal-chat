26ca33bfa380ffca5d3ab65bb80792e603a76e68
luzhong <luzhong@zeaho.com>
2020-07-30 19:40:47 +0800

    feat(genaljoin.vue): 修复登录按钮提示

--- genal-chat-client/src/components/GenalJoin.vue
+++ genal-chat-client/src/components/GenalJoin.vue
@@ -1,43 +1,18 @@
 <template>
   <div class="login">
-    <a-modal 
-      header=''
-      footer=''
-      :visible="showModal"
-      :closable='false'
-    >
-    <a-tabs @change="changeType">
-      <a-tab-pane key="login" tab="登录">
-      </a-tab-pane>
-      <a-tab-pane key="regist" tab="注册" force-render>
-      </a-tab-pane>
-    </a-tabs>
-      <a-form
-        id="components-form-demo-normal-login"
-        :form="form"
-        class="login-form"
-        @submit="handleSubmit"
-      >
+    <a-modal header="" footer="" :visible="showModal" :closable="false">
+      <a-tabs @change="changeType">
+        <a-tab-pane key="login" tab="登录"> </a-tab-pane>
+        <a-tab-pane key="regist" tab="注册" force-render> </a-tab-pane>
+      </a-tabs>
+      <a-form id="components-form-demo-normal-login" :form="form" class="login-form" @submit="handleSubmit">
         <a-form-item>
-          <a-input
-            v-decorator="[
-              'username',
-              { rules: [{ required: true, message: '请输入用户名!' }] },
-            ]"
-            placeholder="username"
-          >
+          <a-input v-decorator="['username', { rules: [{ required: true, message: '请输入用户名!' }] }]" placeholder="用户名">
             <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
           </a-input>
         </a-form-item>
         <a-form-item>
-          <a-input
-            v-decorator="[
-              'password',
-              { rules: [{ required: true, message: '请输入密码!' }] },
-            ]"
-            type="password"
-            placeholder="Password"
-          >
+          <a-input v-decorator="['password', { rules: [{ required: true, message: '请输入密码!' }] }]" type="password" placeholder="密码">
             <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
           </a-input>
         </a-form-item>
@@ -54,7 +29,7 @@
             记住密码
           </a-checkbox>
           <a-button type="primary" html-type="submit" class="login-form-button">
-            登录
+            {{ buttonText }}
           </a-button>
         </a-form-item>
       </a-form>
@@ -64,32 +39,38 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator';
-import {mapMutations, mapGetters} from "vuex";
+import { mapMutations, mapGetters } from 'vuex';
 
 @Component
 export default class GenalJoin extends Vue {
-  @Prop() showModal:boolean;
+  @Prop() showModal: boolean;
   form: any = null;
-  type: string = 'login'
+  type: string = 'login';
+  buttonText: string = '登录';
 
   created() {
     this.form = this.$form.createForm(this, { name: 'normal_login' });
   }
 
   changeType(type: string) {
-    this.type = type
+    this.type = type;
+    if (this.type === 'login') {
+      this.buttonText = '登录';
+    } else if (this.type === 'regist') {
+      this.buttonText = '注册';
+    }
   }
 
-  handleSubmit(e:any) {
+  handleSubmit(e: any) {
     e.preventDefault();
-    this.form.validateFields((err:any, values:User) => {
+    this.form.validateFields((err: any, values: User) => {
       if (!err) {
-        if(this.type === 'regist') {
-          values.createTime = new Date().valueOf()
+        if (this.type === 'regist') {
+          values.createTime = new Date().valueOf();
         }
         // @ts-ignore
-        delete values.remember
-        this.$emit(this.type,values)
+        delete values.remember;
+        this.$emit(this.type, values);
       }
     });
   }
