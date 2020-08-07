4b47d401f915d8af50d2e58678c17afdccd60038
黄汉增 <huanghanzeng@dm-ai.cn>
2020-08-07 15:13:29 +0800

    #20_配置预览插件并修改样式

--- genal-chat-client/src/main.ts
+++ genal-chat-client/src/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VViewer from 'v-viewer';
+import Viewer from 'v-viewer';
 
 Vue.config.productionTip = false;
 
@@ -19,7 +19,23 @@ Vue.prototype.$moment = moment;
 
 // 图片预览插件
 import 'viewerjs/dist/viewer.css';
-Vue.use(VViewer);
+Vue.use(Viewer, {
+  defaultOptions: {
+    navbar: false,
+    toolbar: {
+      zoomIn: 0,
+      zoomOut: 0,
+      oneToOne: 4,
+      reset: 4,
+      prev: 0,
+      next: 0,
+      rotateLeft: 4,
+      rotateRight: 4,
+      flipHorizontal: 4,
+      flipVertical: 4,
+    },
+  },
+});
 
 new Vue({
   router,
